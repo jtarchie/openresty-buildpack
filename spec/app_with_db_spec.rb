fdb1782aa1e9df56949838b02f9d8bbf0dff6a81
JT Archie <jtarchie@gmail.com>
2014-12-31 17:43:02 -0500

    support DATABASE_URL rewrite in nginx.conf

---
+++ spec/app_with_db_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'json'
+
+describe 'When deploying an app with a database' do
+  it 'successfuly serves the endpoint /' do
+    Hatchet::Runner.new('openresty-postgres').deploy do |app|
+      app.add_database
+      expect(`heroku pg:psql -a #{app.name} < dump.sql`).to include 'DROP TABLE'
+      output = `curl http://#{app.name}.herokuapp.com`
+      expect(JSON.parse(output)).to eq [{"id"=>1, "name"=>"Annie"}, {"id"=>2, "name"=>"Bob"}, {"id"=>3, "name"=>"Dave"}]
+    end
+  end
+end
+
+
