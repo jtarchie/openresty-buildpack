0c0351e943485fb6bd0c80d2a935289e192a3311
JT Archie <jtarchie@gmail.com>
2015-01-01 23:45:16 -0500

    Support LuaRocks

---
+++ spec/app_with_luarocks_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'When deploying an app with luarocks dependencies' do
+  it 'successfuly serves the endpoint /luarocks' do
+    Hatchet::GitApp.new('openresty-luarocks', buildpack_url: 'https://github.com/jtarchie/openresty-buildpack.git').deploy do |app|
+      sleep(0.1) while !app.deployed?
+      expect(app.output).to include 'Installing LuaRocks'
+      output = `curl http://#{app.name}.herokuapp.com/luarocks`
+      expect(output).to include "rocks and md5 loaded"
+      expect(output).to include "bar loaded"
+      expect(output).to include "85e73df5c41378f830c031b81e4453d2"
+    end
+  end
+end
+
+
+
